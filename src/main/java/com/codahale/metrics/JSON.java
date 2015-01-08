245e36d71a77d97d5682a094e948f564524a8765
mxro <mxro@nowhere.com>
2015-01-09 09:45:19 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 09:45:19 EST 2015

--- src/main/java/com/codahale/metrics/JSON.java
+++ src/main/java/com/codahale/metrics/JSON.java
@@ -1,6 +1,7 @@
 package com.codahale.metrics;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map.Entry;
 
 public class JSON {
@@ -8,14 +9,33 @@ public class JSON {
     private final List<Entry<String, String>> entries;
 
     public void add(final String key, final Object value) {
-        entries.put(key, value.toString());
+        final String valueStr = value.toString();
+        entries.add(new Entry<String, String>() {
+
+            @Override
+            public String setValue(final String value) {
+                return null;
+            }
+
+            @Override
+            public String getValue() {
+
+                return valueStr;
+            }
+
+            @Override
+            public String getKey() {
+                return key;
+            }
+        });
+
     }
 
     public String render() {
 
         String res = "{\n";
 
-        for (final Entry<String, String> e : entries.entrySet()) {
+        for (final Entry<String, String> e : entries) {
             res += "    " + e.getKey() + ": '" + e.getValue() + "',\n";
         }
 
