2cda8f2d64a630444f0036b74e6d1a72345375d4
mxro <mxro@nowhere.com>
2015-01-09 10:09:51 +1100

    a

---
+++ src/main/java/de/mxro/metrics/internal/operations/HistrogramEvent.java
@@ -0,0 +1,12 @@
+package de.mxro.metrics.internal.operations;
+
+import de.mxro.metrics.helpers.MetricsData;
+
+public class HistrogramEvent extends OperationWithId {
+
+    @Override
+    public void perform(final MetricsData data) {
+
+    }
+
+}
