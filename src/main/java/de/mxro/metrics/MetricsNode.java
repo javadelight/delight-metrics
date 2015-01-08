b0e931f6901ea1dc646686b49e42165e28ab63a8
mxro <mxro@nowhere.com>
2015-01-09 10:59:48 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 10:59:48 EST 2015

--- src/main/java/de/mxro/metrics/MetricsNode.java
+++ src/main/java/de/mxro/metrics/MetricsNode.java
@@ -1,13 +1,15 @@
 package de.mxro.metrics;
 
+import de.mxro.async.Promise;
+import de.mxro.async.callbacks.ValueCallback;
 import de.mxro.metrics.helpers.RecordOperation;
 
 public interface MetricsNode {
 
     public void record(RecordOperation op);
 
-    public <T> T get(String id, Class<T> type);
+    public Promise<Object> retrieve(String id);
 
-    public Object get(String id);
+    public void retrieve(String id, ValueCallback<Object> cb);
 
 }
