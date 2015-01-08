96bd1c59e3e7edae1270e9bdd5dd4216de4a8b47
mxro <mxro@nowhere.com>
2015-01-09 08:25:19 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 08:25:19 EST 2015

--- src/main/java/de/mxro/metrics/internal/MetricsNodeUnsafe.java
+++ src/main/java/de/mxro/metrics/internal/MetricsNodeUnsafe.java
@@ -2,6 +2,8 @@ package de.mxro.metrics.internal;
 
 import java.util.Map;
 
+import com.codahale.metrics.Meter;
+
 import de.mxro.metrics.MetricsNode;
 
 /**
@@ -16,6 +18,12 @@ public class MetricsNodeUnsafe implements MetricsNode {
 
     @Override
     public void meter(final String id) {
+        final Object object = metrics.get(id);
+
+        if (!(object instanceof Meter)) {
+            throw new RuntimeException("Id " + id + " is assigned the incompatible metrics type [" + object.getClass()
+                    + "]");
+        }
 
     }
 
