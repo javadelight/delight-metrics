4b79568be16e42d643b67ad9fb9f991013e03516
mxro <mxro@nowhere.com>
2015-01-09 08:13:39 +1100

    a

--- src/main/java/de/mxro/metrics/Metrics.java
+++ src/main/java/de/mxro/metrics/Metrics.java
@@ -1,9 +1,11 @@
 package de.mxro.metrics;
 
+import de.mxro.metrics.internal.MetricsCollectionImpl;
+
 public class Metrics {
 
     public static MetricsCollection create() {
-
+        return new MetricsCollectionImpl();
     }
 
 }
