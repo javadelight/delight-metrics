296fa65fe1c34615afecd5603b032f2d3c2a79f5
mxro <mxro@nowhere.com>
2015-01-09 08:55:19 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 08:55:19 EST 2015

--- src/main/java/com/codahale/metrics/package-info.java
+++ src/main/java/com/codahale/metrics/package-info.java
@@ -1,6 +1,7 @@
 /**
  * <p>Imported classes from the <a href='https://dropwizard.github.io/metrics/3.1.0/'>Metrics Project</a>. 
  * <p>The classes are slightly modified so that they can be compiled by GWT.
+ * <p>More meaningful .toString implementations are provided as a lightweight way to render the metrics.
  * 
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
