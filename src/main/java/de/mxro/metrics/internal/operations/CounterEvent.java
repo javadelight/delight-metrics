8806f2dfda2a244ac063f2d074b08e76bb28bfdd
mxro <mxro@nowhere.com>
2015-01-09 10:03:19 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 10:03:19 EST 2015

--- src/main/java/de/mxro/metrics/internal/operations/CounterEvent.java
+++ src/main/java/de/mxro/metrics/internal/operations/CounterEvent.java
@@ -6,9 +6,18 @@ import de.mxro.metrics.helpers.MetricsData;
 
 public class CounterEvent extends OperationWithId {
 
+    private final long increment;
+
     @Override
     public void perform(final MetricsData data) {
         final Counter counter = data.get(id, Counter.class);
+
+        counter.inc(increment);
+    }
+
+    public CounterEvent(final long increment) {
+        super();
+        this.increment = increment;
     }
 
 }
