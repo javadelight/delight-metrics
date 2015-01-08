4b5552305e1bb66db873f7954e8bf0b7643ff4b8
mxro <mxro@nowhere.com>
2015-01-09 07:59:13 +1100

    Importing classes from https://github.com/dropwizard/metrics

---
+++ src/main/java/com/codahale/metrics/Counter.java
@@ -0,0 +1,56 @@
+package com.codahale.metrics;
+
+/**
+ * An incrementing and decrementing counter metric.
+ */
+public class Counter implements Metric, Counting {
+    private final LongAdder count;
+
+    public Counter() {
+        this.count = new LongAdder();
+    }
+
+    /**
+     * Increment the counter by one.
+     */
+    public void inc() {
+        inc(1);
+    }
+
+    /**
+     * Increment the counter by {@code n}.
+     *
+     * @param n
+     *            the amount by which the counter will be increased
+     */
+    public void inc(final long n) {
+        count.add(n);
+    }
+
+    /**
+     * Decrement the counter by one.
+     */
+    public void dec() {
+        dec(1);
+    }
+
+    /**
+     * Decrement the counter by {@code n}.
+     *
+     * @param n
+     *            the amount by which the counter will be decreased
+     */
+    public void dec(final long n) {
+        count.add(-n);
+    }
+
+    /**
+     * Returns the counter's current value.
+     *
+     * @return the counter's current value
+     */
+    @Override
+    public long getCount() {
+        return count.sum();
+    }
+}
