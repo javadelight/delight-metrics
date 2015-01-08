e5730dc965d4cfe2379e055c53a7b0088d7eb565
mxro <mxro@nowhere.com>
2015-01-09 10:10:19 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 10:10:19 EST 2015

--- src/main/java/de/mxro/metrics/internal/MetricsFactory.java
+++ src/main/java/de/mxro/metrics/internal/MetricsFactory.java
@@ -1,7 +1,9 @@
 package de.mxro.metrics.internal;
 
 import com.codahale.metrics.Counter;
+import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
+import com.codahale.metrics.UniformReservoir;
 
 public class MetricsFactory {
 
@@ -14,6 +16,10 @@ public class MetricsFactory {
 
         if (type.equals(Counter.class)) {
             return (T) new Counter();
+        }
+
+        if (type.equals(Histogram.class)) {
+            return (T) new Histogram(new UniformReservoir());
         }
 
         throw new RuntimeException("Cannot create metric of type [" + type.getClass() + "]");
