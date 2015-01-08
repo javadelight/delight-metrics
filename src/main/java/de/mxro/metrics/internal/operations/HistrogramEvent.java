d6246a216219e54277e0d52d21767255b886bdf0
mxro <mxro@nowhere.com>
2015-01-09 10:11:19 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 10:11:19 EST 2015

--- src/main/java/de/mxro/metrics/internal/operations/HistrogramEvent.java
+++ src/main/java/de/mxro/metrics/internal/operations/HistrogramEvent.java
@@ -1,12 +1,25 @@
 package de.mxro.metrics.internal.operations;
 
+import com.codahale.metrics.Histogram;
+
 import de.mxro.metrics.helpers.MetricsData;
 
 public class HistrogramEvent extends OperationWithId {
 
+    private final int value;
+
     @Override
     public void perform(final MetricsData data) {
 
+        final Histogram hist = data.get(id, Histogram.class);
+
+        hist.update(value);
+
+    }
+
+    public HistrogramEvent(final int value) {
+        super();
+        this.value = value;
     }
 
 }
