7445794877340830ec6be57ce79bd797caa969b9
mxro <mxro@nowhere.com>
2015-01-09 08:11:07 +1100

    a

---
+++ src/main/java/com/codahale/metrics/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>Imported classes from the <a href='https://dropwizard.github.io/metrics/3.1.0/'>Metrics Project</a>. 
+ * <p>The classes are slightly modified so that they can be compiled by GWT.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+package com.codahale.metrics;
