8f196be119fb5657eac72b03a82322e679be3bb7
mxro <mxro@nowhere.com>
2015-01-09 11:15:51 +1100

    a

---
+++ src/main/java/de/mxro/metrics/MetricsCommon.java
@@ -0,0 +1,31 @@
+package de.mxro.metrics;
+
+import de.mxro.metrics.helpers.RecordOperation;
+import de.mxro.metrics.internal.UnsafeMetricsNode;
+import de.mxro.metrics.internal.operations.CounterEvent;
+import de.mxro.metrics.internal.operations.HistrogramEvent;
+import de.mxro.metrics.internal.operations.MarkEvent;
+
+public class MetricsCommon {
+
+    public static MetricsNode createUnsafe() {
+        return new UnsafeMetricsNode();
+    }
+
+    public static RecordOperation happened(final String id) {
+        return new MarkEvent().setId(id);
+    }
+
+    public static RecordOperation increment(final String id) {
+        return new CounterEvent(1).setId(id);
+    }
+
+    public static RecordOperation decrement(final String id) {
+        return new CounterEvent(-1).setId(id);
+    }
+
+    public static RecordOperation value(final String id, final long value) {
+        return new HistrogramEvent(value).setId(id);
+    }
+
+}
