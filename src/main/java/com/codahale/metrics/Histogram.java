dfe91438f98b1473eca9ea79225aeea258136976
mxro <mxro@nowhere.com>
2015-01-09 12:46:00 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 12:46:00 EST 2015

--- src/main/java/com/codahale/metrics/Histogram.java
+++ src/main/java/com/codahale/metrics/Histogram.java
@@ -71,9 +71,9 @@ public class Histogram implements Metric, Sampling, Counting, JSONSerialization 
         o.add("Standard Deviation", snap.getStdDev());
         o.add("Max", snap.getMax());
         o.add("Min", snap.getMin());
-        o.add("75% of Values Within", snap.get75thPercentile());
-        o.add("95% of Values Within", snap.get95thPercentile());
-        o.add("98% of Values Within", snap.get98thPercentile());
+        o.add("75% of Values Smaller Than", snap.get75thPercentile());
+        o.add("95% of Values Smaller Than", snap.get95thPercentile());
+        o.add("98% of Values Smaller Than", snap.get98thPercentile());
         return o;
     }
 
