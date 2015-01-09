cb1e89b54f62c64cd788519baba456ba58b05956
mxro <mxro@nowhere.com>
2015-01-09 11:01:48 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 11:01:48 EST 2015

--- src/main/java/de/mxro/metrics/internal/UnsafeMetricsNode.java
+++ src/main/java/de/mxro/metrics/internal/UnsafeMetricsNode.java
@@ -1,8 +1,12 @@
 package de.mxro.metrics.internal;
 
+import de.mxro.async.Async;
+import de.mxro.async.Deferred;
+import de.mxro.async.Promise;
+import de.mxro.async.callbacks.ValueCallback;
 import de.mxro.metrics.MetricsNode;
-import de.mxro.metrics.helpers.RecordOperation;
 import de.mxro.metrics.helpers.MetricsData;
+import de.mxro.metrics.helpers.RecordOperation;
 
 /**
  * A non-thread safe implementation of {@link MetricsNode}.
@@ -25,15 +29,20 @@ public class UnsafeMetricsNode implements MetricsNode {
     }
 
     @Override
-    public <T> T get(final String id, final Class<T> type) {
+    public Promise<Object> retrieve(final String id) {
 
-        return data.get(id, type);
+        return Async.promise(new Deferred<Object>() {
+
+            @Override
+            public void get(final ValueCallback<Object> callback) {
+                retrieve(id, callback);
+            }
+        });
     }
 
     @Override
-    public Object get(final String id) {
-
-        return data.get(id);
+    public void retrieve(final String id, final ValueCallback<Object> cb) {
+        cb.onSuccess(data.get(id));
     }
 
 }
