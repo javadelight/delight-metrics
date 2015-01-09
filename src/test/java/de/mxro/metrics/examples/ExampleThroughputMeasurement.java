a7ef2bb54a568f4d1189e685521ae794f3ec53be
mxro <mxro@nowhere.com>
2015-01-09 12:23:00 +1100

    Implementing first version of lightweight metrics Autocommit Fri Jan 09 12:23:00 EST 2015

--- src/test/java/de/mxro/metrics/examples/ExampleThroughputMeasurement.java
+++ src/test/java/de/mxro/metrics/examples/ExampleThroughputMeasurement.java
@@ -4,13 +4,15 @@ import de.mxro.metrics.MetricsNode;
 import de.mxro.metrics.jre.Metrics;
 
 public class ExampleThroughputMeasurement {
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws InterruptedException {
 
         final MetricsNode m = Metrics.create();
 
         m.record(Metrics.happened("process"));
         m.record(Metrics.happened("process"));
         m.record(Metrics.happened("process"));
+
+        Thread.sleep(10000);
 
         m.print();
 
