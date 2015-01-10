0303e2926fe4f649ba5d30486e741257aae59c6b
mxro <mxro@nowhere.com>
2015-01-10 15:54:22 +1100

    Resolving compatability issues with JavaScript Autocommit Sat Jan 10 15:54:22 EST 2015

--- src/main/java/com/codahale/metrics/EWMA.java
+++ src/main/java/com/codahale/metrics/EWMA.java
@@ -69,8 +69,9 @@ public class EWMA {
      *            the time unit of the tick interval
      */
     public EWMA(final double alpha, final long interval, final long nanos) {
-        this.interval = nanos * (interval * 2);
-        System.out.println("int " + interval);
+        this.interval = nanos * (interval * 2); // don't know why x2 necessary
+                                                // here?
+
         this.alpha = alpha;
     }
 
