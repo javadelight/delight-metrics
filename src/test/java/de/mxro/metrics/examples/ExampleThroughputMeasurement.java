6154d7ab928f10b32a0c7a6e63d703693e0a1da3
mxro <mxro@nowhere.com>
2015-01-10 15:51:22 +1100

    Resolving compatability issues with JavaScript Autocommit Sat Jan 10 15:51:22 EST 2015

--- src/test/java/de/mxro/metrics/examples/ExampleThroughputMeasurement.java
+++ src/test/java/de/mxro/metrics/examples/ExampleThroughputMeasurement.java
@@ -11,6 +11,13 @@ public class ExampleThroughputMeasurement {
         m.record(Metrics.happened("process"));
         m.record(Metrics.happened("process"));
         m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
+        m.record(Metrics.happened("process"));
 
         Thread.sleep(10000);
 
