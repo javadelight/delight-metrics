5501250aeeeb5c245184e2d7271449bde76a7531
mxro <mxro@nowhere.com>
2015-01-14 15:05:05 +1100

    Adding documentation for async-log Autocommit Wed Jan 14 15:05:05 EST 2015

--- src/main/java/de/mxro/metrics/internal/operations/CounterEvent.java
+++ src/main/java/de/mxro/metrics/internal/operations/CounterEvent.java
@@ -5,15 +5,17 @@ import com.codahale.metrics.Counter;
 import de.mxro.async.properties.PropertyData;
 import de.mxro.async.properties.operations.PropertyOperationWithId;
 
-public class CounterEvent extends PropertyOperationWithId {
+public class CounterEvent extends PropertyOperationWithId<Long> {
 
     private final long increment;
 
     @Override
-    public void perform(final PropertyData data) {
+    public Long perform(final PropertyData data) {
         final Counter counter = data.get(id, Counter.class);
 
         counter.inc(increment);
+
+        return counter.getCount();
     }
 
     public CounterEvent(final long increment) {
