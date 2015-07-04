70894b0ea62186e87500fe31fda50e270c2669f8
mxro <mxro@nowhere.com>
2015-07-05 08:05:25 +1000

    Moving projects to Java Delight Autocommit Sun Jul 05 08:05:25 EST 2015

--- src/test/java/de/mxro/metrics/tests/TestCounter.java
+++ src/test/java/de/mxro/metrics/tests/TestCounter.java
@@ -2,8 +2,7 @@ package de.mxro.metrics.tests;
 
 import de.mxro.async.properties.PropertyNode;
 import de.mxro.async.properties.PropertyOperation;
-import de.mxro.fn.Success;
-import de.mxro.metrics.jre.Metrics;
+import de.mxro.metrics.MetricsCommon;
 import de.mxro.promise.Promise;
 import de.oehme.xtend.junit.Hamcrest;
 import de.oehme.xtend.junit.JUnit;
@@ -23,20 +22,18 @@ import org.junit.rules.ErrorCollector;
 public class TestCounter {
   @Test
   public void test() {
-    PropertyNode m = Metrics.create();
-    PropertyOperation<Long> _increment = Metrics.increment("de.mxro.counter");
-    m.<Long>record(_increment);
-    PropertyOperation<Long> _increment_1 = Metrics.increment("de.mxro.counter");
-    m.<Long>record(_increment_1);
-    PropertyOperation<Long> _decrement = Metrics.decrement("de.mxro.counter");
-    m.<Long>record(_decrement);
-    Promise<Object> _retrieve = m.retrieve("de.mxro.counter");
+    PropertyNode m = MetricsCommon.createUnsafe();
+    PropertyOperation<Long> _value = MetricsCommon.value("de.mxro.hist", 1);
+    m.<Long>record(_value);
+    PropertyOperation<Long> _value_1 = MetricsCommon.value("de.mxro.hist", 2);
+    m.<Long>record(_value_1);
+    PropertyOperation<Long> _value_2 = MetricsCommon.value("de.mxro.hist", 3);
+    m.<Long>record(_value_2);
+    Promise<Object> _retrieve = m.retrieve("de.mxro.hist");
     Object _get = _retrieve.get();
     String _string = _get.toString();
-    boolean _contains = _string.contains("1");
+    boolean _contains = _string.contains("2.0");
     TestCounter.<Boolean, Boolean>operator_doubleArrow(Boolean.valueOf(_contains), Boolean.valueOf(true));
-    Promise<Success> _stop = m.stop();
-    _stop.get();
   }
   
   private static void assertArrayEquals(final Object[] expecteds, final Object[] actuals) {
